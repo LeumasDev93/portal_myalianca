89856edfa5957b5c6e11d5e2ba98d5a8bbeefafa
LeumasDev93 <cursosdrivecv22@gmail.com>
2025-05-29 01:19:00 -0100

    fix bugs

--- src/app/(private)/(recibo)/recibo/page.tsx
+++ src/app/(private)/(recibo)/recibo/page.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Card,
