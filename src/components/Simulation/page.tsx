ea02e3978d5d67fb19c1cd66fa258fe93c89aa7c
LeumasDev93 <cursosdrivecv22@gmail.com>
2025-07-03 15:28:58 -0100

    melhorias

--- src/components/Simulation/page.tsx
+++ src/components/Simulation/page.tsx
@@ -68,7 +68,7 @@ export default function SimulationScreen() {
       ) : products.length === 0 ? (
         <LoadingScreen />
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
           {products.map((product) => (
             <Card key={product.productId} product={product} />
           ))}
