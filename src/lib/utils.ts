5f6a9f1a1bf99f235f92c40b9c7de608f8c9fc4a
LeumasDev93 <cursosdrivecv22@gmail.com>
2025-07-17 10:20:19 -0100

    melhorias

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -297,8 +297,14 @@ export function getFirstAndLastName(fullName: string): string {
   return `${firstName} ${lastName}`;
 }
 
-export const getSafeGridClass = (input: string = "") => {
-  if (input.includes("grid-cols-3")) return "grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4";
-  if (input.includes("grid-cols-2")) return "grid grid-cols-1 sm:grid-cols-2 gap-4";
+export const getSafeGridClass = (input?: string | null) => {
+  const safeInput = input ?? ""; // usa "" se input for null ou undefined
+
+  if (safeInput.includes("grid-cols-3"))
+    return "grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4";
+
+  if (safeInput.includes("grid-cols-2"))
+    return "grid grid-cols-1 sm:grid-cols-2 gap-4";
+
   return "grid grid-cols-1 gap-4";
 };
