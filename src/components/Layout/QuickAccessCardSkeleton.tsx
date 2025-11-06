9bff07a4e931b90f00258431150e61878ff27d5c
Nelson Andrade <nelson.andrade@alianca.ad>
2025-11-06 14:28:34 -0100

    fix: ajusta estrutura do skeleton para corresponder exatamente ao card real
    - Reorganiza layout: TÃ­tulo e Ãcone na mesma linha (flex justify-between)
    - DescriÃ§Ã£o com flex-1 (ocupa espaÃ§o disponÃ­vel)
    - BotÃ£o com h-[26px] (altura aproximada do botÃ£o real)
    - Altura do tÃ­tulo: h-[14px] sm:h-3 xl:h-5 (proporcional ao texto)
    - Ãcone: w-5 h-5 xl:w-6 xl:h-6 (proporcional ao text-xl)
    - Remove espaÃ§amentos excessivos
    - Skeleton agora replica exatamente a estrutura do card real
    - Zero layout shift ao carregar
    - Totalmente responsivo em todos breakpoints

--- src/components/Layout/QuickAccessCardSkeleton.tsx
+++ src/components/Layout/QuickAccessCardSkeleton.tsx
@@ -3,27 +3,26 @@ import React from "react";
 export default function QuickAccessCardSkeleton() {
   return (
     <div className="flex flex-col border rounded-xl w-60 h-32 xl:h-40 sm:w-[200px] xl:w-[270px] shadow-md p-3 xl:p-4 relative bg-white border-gray-200 animate-pulse">
-      <div className="h-full flex flex-col">
-        {/* Ícone skeleton */}
-        <div className="flex-shrink-0 mb-2">
-          <div className="w-8 h-8 xl:w-10 xl:h-10 bg-gray-200 rounded" />
-        </div>
-
+      {/* Título e ícone na mesma linha - igual ao card real */}
+      <div className="flex justify-between items-center mb-2">
         {/* Título skeleton */}
-        <div className="mb-2">
-          <div className="h-4 xl:h-5 bg-gray-200 rounded w-3/4" />
+        <div className="flex-1 mr-2">
+          <div className="h-[14px] sm:h-3 xl:h-5 bg-gray-200 rounded w-3/4" />
         </div>
+        {/* Ícone skeleton */}
+        <div className="flex-shrink-0 mr-4">
+          <div className="w-5 h-5 xl:w-6 xl:h-6 bg-gray-200 rounded" />
+        </div>
+      </div>
 
-        {/* Descrição skeleton */}
-        <div className="flex-1 space-y-1.5">
-          <div className="h-3 bg-gray-200 rounded w-full" />
-          <div className="h-3 bg-gray-200 rounded w-5/6" />
-        </div>
+      {/* Descrição skeleton */}
+      <div className="flex-1 mb-3">
+        <div className="h-3 xl:h-4 bg-gray-200 rounded w-full" />
+      </div>
 
-        {/* Botão skeleton */}
-        <div className="mt-auto pt-2">
-          <div className="h-8 xl:h-9 bg-gray-200 rounded w-full" />
-        </div>
+      {/* Botão skeleton */}
+      <div className="w-full">
+        <div className="h-[26px] bg-gray-200 rounded-lg w-full" />
       </div>
     </div>
   );
